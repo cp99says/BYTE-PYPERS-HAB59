a6586661a81212bf1f9e6201415b69a0c7df6116
sd2001 <im.swarnabha2001@gmail.com>
2020-08-12 02:25:58 +0530

    Adding the graphical encodings

--- Data Analysis/app.py
+++ Data Analysis/app.py
@@ -1,13 +1,15 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+
 #import data_extraction as de
-import plotly.graph_objs as go
+
 
 app=dash.Dash()
 app.layout=html.Div([
     html.H1("Hello Dash")
 ])
+#app.layout='temp-plot.html'
 
 if __name__=="__main__":
     app.run_server(port="1818",debug=True)
