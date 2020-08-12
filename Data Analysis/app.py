6229912380ab69c68ecaaed729ad72b7a5e82b05
sd2001 <im.swarnabha2001@gmail.com>
2020-08-12 21:09:38 +0530

    Updating the ipynb file and planning out the app layout

--- Data Analysis/app.py
+++ Data Analysis/app.py
@@ -1,38 +1,26 @@
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
+import dash_core_components as dcc
+from plotly.subplots import make_subplots
+#import data_extraction as de
+import plotly.graph_objects as go
+animals=['giraffes', 'orangutans', 'monkeys']
+layout=go.Layout(title='Animals',
+                     width=500,
+                    height=400)
 
-app = dash.Dash()
+fig1 = go.Figure(data=[go.Bar(x=animals, y=[20, 14, 23])],layout=layout)
+fig2 = go.Figure(data=[go.Bar(x=animals, y=[15, 22, 22])],layout=layout)
 
-app.layout = html.Div([
-    dcc.Graph(
-        id = 'graph1',
-        figure = {
-            'data': [
-                { 'x': [1, 2, 3], 'y': [4, 1, 2] },
-            ],
-        }
-    ),
-    dcc.Graph(
-        id='graph2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [6, 5, 4]},
-            ],
-        }
-    ),
-
-    dcc.Graph(
-        id='graph3',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [7, 4, 3]},
-            ],
-        }
-    ),
-
+app=dash.Dash()
+app.layout = html.Div(className='row', children=[
+    html.H1("Data Analysis (Byte PYPERS)"),
+    
+    html.Div(children=[
+        dcc.Graph(figure=fig1,id="graph1", style={'display': 'inline-block'}),
+        dcc.Graph(figure=fig2,id="graph2", style={'display': 'inline-block'})
+    ])
 ])
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True, port=5055)
+ 
+if __name__=="__main__":
+    app.run_server(port="1818",debug=True)
