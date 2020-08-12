9b97e31a5206837b6bd64402be201a5207757abd
sd2001 <im.swarnabha2001@gmail.com>
2020-08-12 17:07:03 +0530

    Adding Pie Charts

--- Data Analysis/app.py
+++ Data Analysis/app.py
@@ -1,15 +1,38 @@
 import dash
-import dash_html_components as html
 import dash_core_components as dcc
+import dash_html_components as html
 
-#import data_extraction as de
+app = dash.Dash()
 
+app.layout = html.Div([
+    dcc.Graph(
+        id = 'graph1',
+        figure = {
+            'data': [
+                { 'x': [1, 2, 3], 'y': [4, 1, 2] },
+            ],
+        }
+    ),
+    dcc.Graph(
+        id='graph2',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [6, 5, 4]},
+            ],
+        }
+    ),
 
-app=dash.Dash()
-app.layout=html.Div([
-    html.H1("Hello Dash")
+    dcc.Graph(
+        id='graph3',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [7, 4, 3]},
+            ],
+        }
+    ),
+
 ])
-#app.layout='temp-plot.html'
 
-if __name__=="__main__":
-    app.run_server(port="1818",debug=True)
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=5055)
