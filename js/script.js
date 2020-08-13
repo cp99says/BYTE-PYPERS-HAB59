8e4b24118bffe27c35912d09e0c7288f77115cec
atimabh <abarunaha@gmail.com>
2020-08-13 11:42:27 +0530

    added user portal

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ $("#customFile").on("change", function () {
   var fileName = $(this).val().replace("C:\\fakepath\\", " ");
   $(this).next(".custom-file-label").html(fileName);
 });
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
+
+fetch("https://byte-pypers.herokuapp.com/get", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error));
