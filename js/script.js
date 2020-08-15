04ed03cc8751fe3904d8a39308185f05d20010b8
atimabh <abarunaha@gmail.com>
2020-08-16 00:49:37 +0530

    slot booking api added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-$("#customFile").on("change", function () {
-  var fileName = $(this).val().replace("C:\\fakepath\\", " ");
-  $(this).next(".custom-file-label").html(fileName);
+$("#customFile").on("change", function() {
+    var fileName = $(this).val().replace("C:\\fakepath\\", " ");
+    $(this).next(".custom-file-label").html(fileName);
 });
 
 // var requestOptions = {
@@ -12,3 +12,48 @@ $("#customFile").on("change", function () {
 //   .then((response) => response.json())
 //   .then((result) => console.log(result))
 //   .catch((error) => console.log("error", error));
+
+// var myHeaders = new Headers();
+// myHeaders.append("Content-Type", "application/json");
+
+// var raw = JSON.stringify({ email: "asd@gmail.com", password: "abc@123456" });
+
+// var requestOptions = {
+//   method: "POST",
+//   headers: myHeaders,
+//   body: raw,
+//   redirect: "follow",
+// };
+
+// fetch("https://byte-pypers-auth.herokuapp.com/customer/login", requestOptions)
+//   .then((response) => response.text())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log("error", error));
+
+
+
+
+// var myHeaders = new Headers();
+// myHeaders.append("Content-Type", "application/json");
+
+// var raw = JSON.stringify({
+//     name: "atimabh test 2",
+//     contact_number: "8802216912",
+//     date: "15/07/20",
+//     time: "04:00 P.M.",
+// });
+
+// var requestOptions = {
+//     method: "POST",
+//     headers: myHeaders,
+//     body: raw,
+//     redirect: "follow",
+// };
+
+// fetch(
+//         "https://byte-pypers-auth.herokuapp.com/customer/select_slot",
+//         requestOptions
+//     )
+//     .then((response) => response.text())
+//     .then((result) => console.log(result))
+//     .catch((error) => console.log("error", error));
---
+++ js/store.js
@@ -0,0 +1,27 @@
+document.getElementById("slotSubmit").addEventListener('click', () => {
+
+    var raw = JSON.stringify({
+        name: document.getElementById("slotName").value,
+        contact_number: document.getElementById("slotMobile").value,
+        date: document.getElementById("slotDate").value,
+        time: document.getElementById("slotTime").value
+    });
+
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Content-Type", "application/json");
+
+    console.log(raw);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://byte-pypers-auth.herokuapp.com/customer/select_slot", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+});
