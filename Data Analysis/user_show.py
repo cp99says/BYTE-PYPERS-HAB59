003ff8c903eedbe326d53f342d6849b0359c244a
sd2001 <im.swarnabha2001@gmail.com>
2020-08-17 02:29:36 +0530

    Adding proper margins in the layout

--- Data Analysis/user_show.py
+++ Data Analysis/user_show.py
@@ -1,12 +1,12 @@
 from user import monthly_sales
 import os
 
-if os.path.exists('u.html'):
-    os.remove("u.html")  
+if os.path.exists('templates/u.html'):
+    os.remove("templates/u.html")  
 
 ms1,ms2=monthly_sales('b100')
 
-with open('u.html','w') as f:
+with open('templates/u.html','w') as f:
     for i in range(len(ms1)):
         f.write(ms1[i].to_html(full_html=False))
         f.write(ms2[i].to_html(full_html=False))
--- Data Analysis/vendor_show.py
+++ Data Analysis/vendor_show.py
@@ -1,14 +1,14 @@
 from vendor import listed_products,monthly_sales
 import os
 
-if os.path.exists('v.html'):
-    os.remove("v.html") 
+if os.path.exists('templates/v.html'):
+    os.remove("templates/v.html") 
 
 lp1,lp2=listed_products('v100')
 ms1,ms2=monthly_sales('v100')
 
 
-with open('v.html','w') as f:
+with open('templates/v.html','w') as f:
     for i in range(len(lp1)):
         f.write(lp1[i].to_html(full_html=False))
         f.write(lp2[i].to_html(full_html=False))
